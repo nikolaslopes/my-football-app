95f27dcfc2b5007200e09c89ba0bdc946377934f
nikolaslopes <nikolaslopes.dev@gmail.com>
2024-06-29 16:48:16 -0400

    build: add env vars

--- src/services/client/httpClient.ts
+++ src/services/client/httpClient.ts
@@ -2,8 +2,14 @@ import axios from "axios";
 
 import { sleep } from "../utils/sleep";
 
+const apiUrl = process.env.REACT_APP_API_URL;
+const apiToken = process.env.REACT_APP_API_TOKEN;
+
 export const httpClient = axios.create({
-	baseURL: "http://localhost:8080",
+	baseURL: apiUrl,
+	headers: {
+		"X-Auth-Token": apiToken,
+	},
 });
 
 httpClient.interceptors.response.use(async (data) => {
